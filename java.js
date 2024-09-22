f72be6625aeaa286bcc8a12b85d620340999e95c
Abdullah Mert <77667846+abdullah1mert@users.noreply.github.com>
2024-09-22 11:40:30 +0300

    .

---
+++ java.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function () {
+    var loader = document.getElementById('loader');
+    var content = document.getElementById('content');
+
+    loader.style.display = 'none';
+    content.style.display = 'block';
+});
+
+
